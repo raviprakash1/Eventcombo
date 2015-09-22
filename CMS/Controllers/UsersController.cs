e72ce94cd37f415f9f298d982e4930b24705b547
Navrit Singh <navrit.singh@kiwitech.com>
2015-09-22 15:54:34 +0530

    User Template creation

--- CMS/Controllers/UsersController.cs
+++ CMS/Controllers/UsersController.cs
@@ -12,33 +12,63 @@ namespace CMS.Controllers
         // GET: Users
         public ActionResult Users()
         {
-            DataTable dt = new DataTable();
-            dt.Columns.Add("UserName", typeof(string));
-            dt.Columns.Add("Email", typeof(string));
-            DataRow dr = dt.NewRow();
-            dr["UserName"] = "Satname ";
-            dr["Email"] = "def@gmail.com";
-            dt.Rows.Add(dr);
+            //UsersContext objUCon = new UsersContext();
+            //UsersTemplate objUTemp = objUCon.UserTemplates;
 
-            dr = dt.NewRow();
-            dr["UserName"] = "Ji";
-            dr["Email"] = "def@gmail.com";
-            dt.Rows.Add(dr);
+            //  List<>
+            //DataTable dt = new DataTable();
+            //dt.Columns.Add("UserName", typeof(string));
+            //dt.Columns.Add("Email", typeof(string));
+            //DataRow dr = dt.NewRow();
+            //dr["UserName"] = "Satname ";
+            //dr["Email"] = "def@gmail.com";
+            //dt.Rows.Add(dr);
+
+            //dr = dt.NewRow();
+            //dr["UserName"] = "Ji";
+            //dr["Email"] = "def@gmail.com";
+            //dt.Rows.Add(dr);
 
 
-            ViewData.Model = dt.AsEnumerable();
+            //ViewData.Model = dt.AsEnumerable();
 
-            UsersTemplate objUser = new UsersTemplate();
-            //AspNetUser UserEntity = new AspNetUser();
-            objUser.UserName = "Satnam";
-            List<UsersTemplate> objUserList = new List<UsersTemplate>();
+            //UsersTemplate objUser = new UsersTemplate();
+            //  objUser.UserName = "Satnam Waheguru";
+            //objUser.EMail = "abc@yahoo.com";
 
-            objUserList.Add(objUser);
-            objUser = new UsersTemplate();
-            objUser.UserName = "Waheguru";
-            objUserList.Add(objUser);
-            ViewBag.UserList = objUserList; 
-            return View();
+
+            //List<UsersTemplate> objUserList = new List<UsersTemplate>();
+
+
+            //objUserList.Add(objUser);
+            //objUser = new UsersTemplate();
+            //objUser.UserName = "Waheguru ji";
+            //objUser.EMail = "cde@yahoo.com";
+            //objUserList.Add(objUser);
+            //    ViewBag.UserList = objUserList;
+
+            return View(GetAllUsers());
+        }
+        public List<UsersTemplate> GetAllUsers()
+        {
+
+            using (EmsEntities objEntity = new EmsEntities())
+            {
+                var modelUserTemp = (from UserTemp in objEntity.AspNetUsers
+                                    select new UsersTemplate
+                                    {
+                                        EMail = UserTemp.Email,
+                                        UserName = UserTemp.UserName
+                                    }
+                                    );
+
+
+                return modelUserTemp.ToList();
+
+            }
+
+
+
         }
     }
 }
