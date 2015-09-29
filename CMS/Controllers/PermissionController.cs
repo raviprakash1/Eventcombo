6c1e59fda6f27a9d65a78c5d39c95133b11d326f
Navrit Singh <navrit.singh@kiwitech.com>
2015-09-29 12:25:13 +0530

    merging

---
+++ CMS/Controllers/PermissionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CMS.Models;
+
+namespace CMS.Controllers
+{
+    public class PermissionController : Controller
+    {
+        // GET: Permission
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public string GetPermission(int iScreenId)
+        //{
+        //    string strPer;
+        //    EmsEntities objEms = new EmsEntities();
+        //    //int iGet = 
+
+        //    //return strPer;
+
+        //}
+                                    
+        //<label for="chk_3">Users</label>
+        //<label for="chk_4">Events</label>
+        //<label for="chk_5">Tickets</label>
+        //<label for="chk_6">Emails</label>
+        //<label for="chk_7">Messages</label>
+    }
+}
