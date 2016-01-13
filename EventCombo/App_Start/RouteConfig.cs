066bad0faf725291fbf54947e7385605f939406e
navritsingh <navrit.singh@kiwitech.com>
2016-01-13 21:18:26 +0530

    Point 7,8

--- EventCombo/App_Start/RouteConfig.cs
+++ EventCombo/App_Start/RouteConfig.cs
@@ -12,9 +12,9 @@ namespace EventCombo
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapMvcAttributeRoutes();
+           // routes.MapMvcAttributeRoutes();
             routes.LowercaseUrls = true;
-            AreaRegistration.RegisterAllAreas();
+            //AreaRegistration.RegisterAllAreas();
             //AreaRegistration.RegisterAllAreas();
             //routes.MapRoute(
             //name: "ViewEvent",
