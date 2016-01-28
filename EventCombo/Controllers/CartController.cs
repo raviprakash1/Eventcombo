a19b59c27f6c452efcfb5287b0bf1e9dc51e11da
navritsingh <navrit.singh@kiwitech.com>
2016-01-28 13:17:07 +0530

    Paypal Changes

--- EventCombo/Controllers/CartController.cs
+++ EventCombo/Controllers/CartController.cs
@@ -8,37 +8,73 @@ namespace EventCombo.Controllers
 {
     public class CartController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult Pay()
+        [HttpGet]
+        public JsonResult PayPaltoken()
         {
             string strGUID = (Session["TicketLockedId"] != null ? Session["TicketLockedId"].ToString() : "");
             using (var objContent = new EventComboEntities())
             {
-               // var EventOrderDetail = (from Order in objContent.Ticket_Purchased_Detail where Order.TPD_GUID == strGUID select Order).FirstOrDefault();
-               // var OrderDetail = (from Orderd in objContent.Order_Detail_T where Orderd.O_Order_Id == EventOrderDetail.TPD_Order_Id select Orderd).FirstOrDefault();
+                // var EventOrderDetail = (from Order in objContent.Ticket_Purchased_Detail where Order.TPD_GUID == strGUID select Order).FirstOrDefault();
+                // var OrderDetail = (from Orderd in objContent.Order_Detail_T where Orderd.O_Order_Id == EventOrderDetail.TPD_Order_Id select Orderd).FirstOrDefault();
+                TicketPayment TicketPayment = new TicketPayment();
+                TicketPayment = (TicketPayment)Session["TicketDatamodel"];
                 PayPalOrder objPay = new PayPalOrder();
-                objPay.Amount = 100;
-                objPay.OrderId ="OT" ;
+                objPay.Amount = Convert.ToDecimal(TicketPayment.strGrandTotal);
+                objPay.OrderId = "";
 
-                //objPay.Amount = OrderDetail.O_OrderAmount;
-                //objPay.OrderId = OrderDetail.O_Order_Id;
-                //
-                //objPay.ReturnUrl = "http://localhost:10159//" + Url.Action("PayPalReturn", "PayPalReturn");
-                //objPay.CancelUrl = "http://localhost:10159//" + Url.Action("PayPalReturn", "PayPalReturn");
-
-                objPay.ReturnUrl = "http://eventcombonew-qa.kiwireader.com//" + Url.Action("PayPalReturn", "PayPalReturn");
-                objPay.CancelUrl = "http://eventcombonew-qa.kiwireader.com//" + Url.Action("PayPalReturn", "PayPalReturn");
-
-                //objPay.ReturnUrl = "http://eventcombo.kiwireader.com//" + Url.Action("PaymentConfirmation", "TicketPayment");
-                //objPay.CancelUrl = "http://eventcombo.kiwireader.com//" + Url.Action("PaymentConfirmation", "TicketPayment");
                 PayPalRedirect redirect = PayPal.ExpressCheckout(objPay);
-                //Session["token"] = redirect.Token;
-                return new RedirectResult(redirect.Url);
+
+                return Json(redirect.Token, JsonRequestBehavior.AllowGet);
             }
+        }
+        public ActionResult CheckoutReview(string token, string PayerID)
+        {
+            string retMsg = "";
+            string PayerID1 = "";
+            TicketPayment TicketPayment = new TicketPayment();
+            TicketPayment = (TicketPayment)Session["TicketDatamodel"];
+            PayPal.GetCheckoutDetails(token, ref PayerID1, ref retMsg);
+            ViewData["ReturnMessage"] = retMsg;
+            ViewData["token"] = token;
+            ViewData["PayerID"] = PayerID1;
+            ViewData["Amount"] = TicketPayment.strGrandTotal;
+            return View();
+        }
+        public ActionResult DoCheckoutPayment(string token, string PayerID)
+        {
+            string retMsg = "";
+            TicketPayment TicketPayment = new TicketPayment();
+            TicketPayment = (TicketPayment)Session["TicketDatamodel"];
+
+            if (PayPal.DoCheckoutPayment(TicketPayment.strGrandTotal, token, PayerID, ref retMsg))
+            {
+                ViewData["ReturnMessage"] = "";
+
+                ViewData["token"] = token;
+                ViewData["PayerID"] = PayerID;
+                ViewData["TRANSACTIONID"] = retMsg;
+                ViewData["Amount"] = TicketPayment.strGrandTotal;
+            }
+            else
+            {
+                ViewData["ReturnMessage"] = retMsg;
+            }
+            return View();
+        }
+
+        public string SetSessionPaypaldetail(TicketPayment model)
+        {
+            string strResult = "";
+            try
+            {
+                strResult = "Y";
+                Session["TicketDatamodel"] = model;
+            }
+            catch (Exception)
+            {
+                strResult = "N";
+            }
+            return strResult;
         }
     }
 }
