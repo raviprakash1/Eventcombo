1f585463ea37957549f54ec1045da5bec9f0dc46
navritsingh <navrit.singh@kiwitech.com>
2016-04-20 15:22:44 +0530

    CMS issues

--- CMS/Controllers/ManageEventController.cs
+++ CMS/Controllers/ManageEventController.cs
@@ -17,6 +17,10 @@ namespace CMS.Controllers
         [HttpPost]
         public ActionResult Index(string SearchStringEventTitle, string EventType, string ddlEventCategory, string ddlEventSubCategory, string Features, string PageF, string Events, string Tickets)
         {
+            if ((Session["UserID"] == null))
+            {
+                return RedirectToAction("Login", "Home");
+            }
             List<EventCreation> objlst = GetAllEvents(SearchStringEventTitle, EventType, ddlEventCategory, ddlEventSubCategory, Features, Events, Tickets);
             if (objlst.Count == 0)
                 ViewData["SearchedUser"] = 0;
@@ -101,6 +105,11 @@ namespace CMS.Controllers
 
         public ActionResult Index()
         {
+            if ((Session["UserID"] == null))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
             List<EventCreation> objlst = GetAllEvents("", "", "", "", "", "", "");
             int iCount = 0;
             List<SelectListItem> PageFilter = new List<SelectListItem>();
