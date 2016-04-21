c7b2c34be12041a9da434cc20897625277a02dd9
Kannan K R <kannan.k@kiwitech.com>
2016-04-21 15:56:56 +0530

    Added new TimeZone Conversion util

---
+++ EventCombo/Utils/DateTimeWithZone.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventCombo.Utils
+{
+    public class DateTimeWithZone
+    {
+        private readonly DateTime utcDateTime;
+        private readonly TimeZoneInfo timeZone;
+
+        public DateTimeWithZone(DateTime dateTime, TimeZoneInfo timeZone)
+        {
+            utcDateTime = TimeZoneInfo.ConvertTimeToUtc(DateTime.SpecifyKind(dateTime, DateTimeKind.Unspecified), timeZone);
+            this.timeZone = timeZone;
+        }
+
+        public DateTimeWithZone(DateTime dateTime, TimeZoneInfo timeZone, bool isUTC)
+        {
+            if (!isUTC)
+            {
+                utcDateTime = TimeZoneInfo.ConvertTimeToUtc(DateTime.SpecifyKind(dateTime, DateTimeKind.Unspecified), timeZone);
+            }
+            else
+            {
+                utcDateTime = dateTime;
+            }
+            this.timeZone = timeZone;
+        }
+
+        public DateTime UniversalTime { get { return utcDateTime; } }
+
+        public TimeZoneInfo TimeZone { get { return timeZone; } }
+
+        public DateTime LocalTime
+        {
+            get
+            {
+                return TimeZoneInfo.ConvertTime(utcDateTime, timeZone);
+            }
+        }
+    }
+}
