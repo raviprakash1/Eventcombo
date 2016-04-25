ff4868eb6c13ef91ad5de7176fff5cd7459e32a6
ShwetaSindhu <shweta.sindhu@kiwitech.com>
2016-04-25 14:21:23 +0530

    changes in create event

--- EventCombo/Controllers/ManageEventController.cs
+++ EventCombo/Controllers/ManageEventController.cs
@@ -1331,8 +1331,8 @@ namespace EventCombo.Controllers
                         //    objEnt.Addresses.Add(objAdd);
                         //}
                     }
-                    DateTimeWithZone dtzstart, dtzend;
-                                       var vEventVenue = (from myEnt in objEnt.EventVenues where myEnt.EventID == Eventid select myEnt).ToList();
+                   
+                var vEventVenue = (from myEnt in objEnt.EventVenues where myEnt.EventID == Eventid select myEnt).ToList();
                     if (vEventVenue != null)
                     {
                         EventVenue objEVenue = new EventVenue();
