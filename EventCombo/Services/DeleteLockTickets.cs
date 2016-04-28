df7811dc08ceacbf721982aacbb5ce10d9159655
navritsingh <navrit.singh@kiwitech.com>
2016-04-28 14:09:03 +0530

    Multiple event changes

---
+++ EventCombo/Services/DeleteLockTickets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EventCombo.Models;
+namespace EventCombo.Services
+{
+    public class DeleteLockTickets
+    {
+        public void Delete()
+        {
+            using (EventComboEntities db = new EventComboEntities())
+            {
+                string strSql = "delete ev set ev.EventStatus = 'Expired' FROM Event ev LEFT Join MultipleEvent me on ev.EventID = me.EventID  where me.M_StartTo < getutcdate() AND LOWER(EventStatus)='live'";
+                db.Database.ExecuteSqlCommand(strSql);
+                db.SaveChanges();
+            }
+        }
+    }
+}
