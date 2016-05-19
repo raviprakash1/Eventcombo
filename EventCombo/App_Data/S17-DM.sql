c862df4a67bc665058525a9eb011e4b53bc73175
Dmitry Sysoev <sisoev74@gmail.com>
2016-05-20 01:02:08 +0300

    First commit for EC1-214

---
+++ EventCombo/App_Data/S17-DM.sql
@@ -0,0 +1,4 @@
+SET IDENTITY_INSERT Permission_Detail ON
+INSERT INTO Permission_Detail (Permission_Id, Permission_Desc, Permission_Category)
+VALUES (13, 'Ticket Orders', 'CMS') 
+SET IDENTITY_INSERT Permission_Detail OFF
