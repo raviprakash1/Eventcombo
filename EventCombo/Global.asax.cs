2cb02d29203029eae179605bea4cba0e85b1c73d
Dmitry Sisoev <sisoev74@gmail.com>
2016-07-04 18:12:52 +0300

    Added global exception handling using NLog (http://nlog-project.org/). To allow sending of mail just uncomment configuration strings in the NLog.config and set right address to send mail.

--- EventCombo/Global.asax.cs
+++ EventCombo/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,17 +9,22 @@ using System.Web.Routing;
 
 namespace EventCombo
 {
-    public class MvcApplication : System.Web.HttpApplication
+  public class MvcApplication : System.Web.HttpApplication
+  {
+    private static Logger logger = LogManager.GetCurrentClassLogger();
+
+    protected void Application_Start()
     {
-       
-
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-        }
+      AreaRegistration.RegisterAllAreas();
+      FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
     }
+    protected void Application_Error(Object sender, EventArgs e)
+    {
+      var raisedException = Server.GetLastError();
+      if (raisedException != null)
+        logger.Error(raisedException, "Exception occured.");
+    }
+  }
 }
