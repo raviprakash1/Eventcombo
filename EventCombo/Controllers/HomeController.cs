4f3497ed376d544ae458e144ccad00719eb046cd
Rahul <rahul.maurya@kiwitech.com>
2016-07-07 17:49:38 +0530

    Added Footer Action method

--- EventCombo/Controllers/HomeController.cs
+++ EventCombo/Controllers/HomeController.cs
@@ -2488,6 +2488,48 @@ namespace EventCombo.Controllers
             CookieStore.SetCookie("ckHeader", header, TimeSpan.FromDays(1));
             //Session["Header"] = header;
         }
+
+        public ActionResult Footer()
+        {
+            Footer footer = new Footer();
+            string User = "";
+            string UserLatitude = "";
+            string UserLongitude = "";
+
+            if (Session["AppId"] != null)
+            {
+                User = Session["AppId"].ToString();
+            }
+            var aspuser = db.Profiles.FirstOrDefault(i => i.UserID == User);
+            if (aspuser != null)
+            {
+                var city = db.Cities.FirstOrDefault(c => c.CityName == aspuser.City);
+                if (city != null)
+                {
+                    UserLatitude = city.Latitude;
+                    UserLongitude = city.Longitude;
+                }
+            }
+            ViewData["UserLatitude"] = UserLatitude;
+            ViewData["UserLongitude"] = UserLongitude;
+
+            var businessPages = db.BusinessPages.Where(x => x.IsOnFooter == true).OrderBy(x => x.PageOrder).ToList();
+            if (businessPages != null)
+            {
+                footer.BusinessPages = businessPages;
+            }
+            var eventTypes = db.EventTypes.Where(x => x.IsOnFooter == true).OrderBy(x => x.EventType1).ToList();
+            if (eventTypes != null)
+            {
+                footer.EventTypes = eventTypes;
+            }
+            var cities = db.Cities.Where(x => x.IsOnFooter == true).OrderBy(x => x.CityName).ToList();
+            if (cities != null)
+            {
+                footer.Cities = cities;
+            }
+            return PartialView("_AngularFooter", footer);
+        }
         
     }
 
