4c8ba7f18704803a5995d93d29158b95c2824136
Rahul <rahul.maurya@kiwitech.com>
2016-07-18 12:13:53 +0530

    Removed SendErrorReport method

--- EventCombo/Utils/Email.cs
+++ EventCombo/Utils/Email.cs
@@ -1,10 +1,8 @@
-﻿using EventCombo.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Net.Mail;
-using System.Threading.Tasks;
 using System.Web;
 
 namespace EventCombo.Utils
@@ -67,124 +65,6 @@ namespace EventCombo.Utils
 
             }
             return sendStatus;
-        }
-        public static void SendErrorReport(string TemplateTag, string PrecedingPage, string ErrorPage, string SpecificErrorReason)
-        {
-            EventComboEntities db = new EventComboEntities();
-            string Subject = string.Empty;
-            string Body = string.Empty;
-            string CC = string.Empty;
-            string BCC = string.Empty;
-            string To = string.Empty;
-            string From = string.Empty;
-            string FromName;
-
-            var EmailTag = db.Email_Tag.ToList();
-            var Emailtemplate = db.Email_Template.Where(x => x.Template_Tag == TemplateTag).SingleOrDefault();
-
-            if (Emailtemplate != null)
-            {
-                if (!string.IsNullOrEmpty(Emailtemplate.To))
-                {
-                    To = Emailtemplate.To;
-                }
-                if (!(string.IsNullOrEmpty(Emailtemplate.From)))
-                {
-                    From = Emailtemplate.From;
-                }
-                else
-                {
-                    From = ConfigurationManager.AppSettings.Get("DefaultEmail");
-                }
-                if (!(string.IsNullOrEmpty(Emailtemplate.From_Name)))
-                {
-                    FromName = Emailtemplate.From_Name;
-                }
-                else
-                {
-                    FromName = From;
-                }
-                if (!(string.IsNullOrEmpty(Emailtemplate.CC)))
-                {
-                    CC = Emailtemplate.CC;
-                }
-                if (!(string.IsNullOrEmpty(Emailtemplate.Bcc)))
-                {
-                    BCC = Emailtemplate.Bcc;
-                }
-                if (!(string.IsNullOrEmpty(Emailtemplate.From_Name)))
-                {
-                    FromName = Emailtemplate.From_Name;
-                }
-                else
-                {
-                    FromName = From;
-                }
-                if (!string.IsNullOrEmpty(Emailtemplate.Subject))
-                {
-                    Subject = Emailtemplate.Subject;
-
-                }
-                if (!string.IsNullOrEmpty(Emailtemplate.TemplateHtml))
-                {
-                    Body = HttpUtility.HtmlDecode(Emailtemplate.TemplateHtml).ToString();
-                    foreach (var emailTag in EmailTag)
-                    {
-                        if (Body.Contains("¶¶" + emailTag.Tag_Name.Trim() + "¶¶"))
-                        {
-                            if (emailTag.Tag_Name == "PrecedingPage")
-                            {
-                                Body = Body.Replace("¶¶PrecedingPage¶¶", PrecedingPage);
-                            }
-                            if (emailTag.Tag_Name == "ErrorPage")
-                            {
-                                Body = Body.Replace("¶¶ErrorPage¶¶", ErrorPage);
-                            }
-                            if (emailTag.Tag_Name == "SpecificErrorReason")
-                            {
-                                Body = Body.Replace("¶¶SpecificErrorReason¶¶", SpecificErrorReason);
-                            }
-                        }
-                    }
-                }
-                try
-                {
-                    using (MailMessage mailMessage = new MailMessage())
-                    {
-                        mailMessage.From = new MailAddress(From, FromName);
-                        mailMessage.Subject = Subject;
-                        mailMessage.Body = Body;
-                        if (!string.IsNullOrEmpty(CC))
-                        {
-                            mailMessage.CC.Add(CC);
-                        }
-                        if (!string.IsNullOrEmpty(BCC))
-                        {
-                            mailMessage.Bcc.Add(BCC);
-                        }
-                        mailMessage.IsBodyHtml = true;
-                        mailMessage.To.Add(new MailAddress(To));
-                        SmtpClient smtp = new SmtpClient();
-                        smtp.Host = ConfigurationManager.AppSettings["Host"];
-                        smtp.EnableSsl = Convert.ToBoolean(ConfigurationManager.AppSettings["EnableSsl"]);
-                        System.Net.NetworkCredential NetworkCred = new System.Net.NetworkCredential();
-                        string s = ConfigurationManager.AppSettings["UserName"];
-                        if (!String.IsNullOrEmpty(s))
-                        {
-                            NetworkCred.UserName = ConfigurationManager.AppSettings["UserName"];
-                            NetworkCred.Password = ConfigurationManager.AppSettings["Password"];
-                        }
-                        smtp.UseDefaultCredentials = true;
-                        smtp.Credentials = NetworkCred;
-                        smtp.Port = int.Parse(ConfigurationManager.AppSettings["Port"]);
-                        smtp.Send(mailMessage);
-                    }
-                }
-                catch (Exception)
-                {
-
-                }
-            }
         }
     }
 }
