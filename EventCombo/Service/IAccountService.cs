c40800bac5eaae351492a64303b7c9d120ab9709
Dmitry Sisoev <sisoev74@gmail.com>
2016-08-12 01:29:26 +0300

    EC1-317. Initial implementation of login/logout/signup features.

---
+++ EventCombo/Service/IAccountService.cs
@@ -0,0 +1,19 @@
+﻿using EventCombo.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventCombo.Service
+{
+  public interface IAccountService
+  {
+    bool CheckUserName(string userName);
+    bool RegisterLoginAttempt(string userId, string ipAddress);
+    void RegisterLogout(string userId);
+    bool RegisterNewUser(string userId, RegisterUserRequestViewModel user, string ipAddress);
+  }
+}
