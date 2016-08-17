987b3d3af23f79eb5f0f11365445559ed9663099
Rahul <rahul.maurya@kiwitech.com>
2016-08-17 17:46:47 +0530

    EC1-304. Sync v_OrderList from staging database

--- EventCombo/App_Data/SQLQuery_EC1-304_OrderAmount.sql
+++ EventCombo/App_Data/SQLQuery_EC1-304_OrderAmount.sql
@@ -9,12 +9,13 @@ GO
 ALTER VIEW [dbo].[v_OrderList]
 AS
 
-SELECT od.O_Id OId, od.O_Order_Id OrderId, od.O_First_Name FirstName, od.O_Last_Name LastName, od.O_Email Email,
+SELECT ISNULL(od.O_Id, 0) OId, od.O_Order_Id OrderId, od.O_First_Name FirstName, od.O_Last_Name LastName, od.O_Email Email,
   pr.City, pr.State, od.O_OrderDateTime OrderDateTime, e.EventTitle Name, od.O_TotalAmount TotalPaid, tpdsum.Quantity, 
   CAST(ped.PE_Scheduled_Date AS DateTime) EventStartDate,
   CAST(CASE WHEN ISNULL(ped.PE_MultipleVenue_id, 0) > 0 THEN ped.PE_Scheduled_Date ELSE ev.EventEndDate END AS DateTime) EventEndDate,
   CAST(CASE WHEN UPPER(e.EventCancel) = 'Y' THEN 1 ELSE 0 END as bit) EventCancelled,
-  ISNULL(od.OrderStateId, 1) OrderStateId, os.OrderStateName, e.EventID, od.O_User_Id UserId
+  ISNULL(od.OrderStateId, 1) OrderStateId, os.OrderStateName, e.EventID, od.O_User_Id UserId,
+  ISNULL(CAST(CASE WHEN ef.FavId is null THEN 0 ELSE 1 END as bit), 0) Favorite
 FROM Order_Detail_T od
 LEFT JOIN 
   (
@@ -28,7 +29,7 @@ LEFT JOIN Publish_Event_Detail ped ON ped.PE_Id = tqd.TQD_PE_Id
 LEFT JOIN [Profile] pr ON pr.UserID = od.O_User_Id
 LEFT JOIN EventVenue  ev ON ev.EventVenueID = ped.PE_SingleVenue_Id
 LEFT JOIN OrderState os ON ISNULL(od.OrderStateId, 1) = os.OrderStateId
-
+LEFT JOIN EventFavourite ef ON ef.eventId = e.EventID AND ef.UserID = od.O_User_Id
 GO
 
 
