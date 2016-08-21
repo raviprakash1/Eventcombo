4711d53d02c0fc74dbaac1ad943eb2dbc060f36e
Dmitry Sisoev <sisoev74@gmail.com>
2016-08-21 23:15:39 +0300

    Sumesh changes

--- EventCombo/App_Data/SQLQuery_EC1-304_EventTicket_View.sql
+++ EventCombo/App_Data/SQLQuery_EC1-304_EventTicket_View.sql
@@ -1,12 +1,17 @@
-/****** Object:  View [dbo].[EventTicket_View]    Script Date: 19-Aug-16 10:20:47 AM ******/
+IF  EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[dbo].[EventTicket_View]'))
+DROP VIEW [dbo].[EventTicket_View]
+GO
+
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
+IF NOT EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[dbo].[EventTicket_View]'))
+EXEC dbo.sp_executesql @statement = N'
 
-ALTER VIEW [dbo].[EventTicket_View]
+CREATE VIEW [dbo].[EventTicket_View]
 AS
 SELECT 
 	Ticket_Purchased_Detail.TPD_Id,
@@ -43,6 +48,7 @@ INNER JOIN Profile ON Profile.UserID = Ticket_Purchased_Detail.TPD_User_Id
 INNER JOIN OrderState ON ISNULL(Order_Detail_T.OrderStateId, 1) = OrderState.OrderStateId
 LEFT OUTER JOIN Promo_Code ON Promo_Code.PC_id=Ticket_Purchased_Detail.TPD_PromoCodeID
 
+' 
 GO
 
 
