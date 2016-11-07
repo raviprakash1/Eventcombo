cbba532b42cc8566e3552102dd623f0d5b8fa5c4
Rahul <rahul.maurya@kiwitech.com>
2016-11-07 16:45:09 +0530

    EC1-356. Added customer detail and address and attendee phone number

--- CMS/Models/AccountViewModels.cs
+++ CMS/Models/AccountViewModels.cs
@@ -228,7 +228,11 @@ namespace CMS.Models
       public long OId { get; set; }
       public string OrderId { get; set; }
       public string Payment { get; set; }
+      public string Name { get; set; }
       public string Email { get; set; }
+      public string PhoneNumber { get; set; }
+      public string Address { get; set; }
+      public string MailingAddress { get; set; }
       public string EventDate { get; set; }
       public string EventLocation { get; set; }
       public string TicketNames { get; set; }
