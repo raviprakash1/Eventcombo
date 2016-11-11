d28c65b9f0e5450690c72031e3ff81e26ff21f90
Rahul <rahul.maurya@kiwitech.com>
2016-11-11 14:51:03 +0530

    EC1-327. Added js files

---
+++ EventCombo/Scripts/AMaterial/Service/OrganizerService.js
@@ -0,0 +1,25 @@
+﻿eventComboApp.service("organizerService", function ($http) {
+
+    this.getOrganizerEvent = function (isUE, oId) {
+        var response = $http({
+            method: "get",
+            datatype: "data.json",
+            url: "/OrganizerInfo/OrganizerEvent",
+            params: {
+                isUpcomingEvent: isUE,
+                organizerId: oId
+            }
+        });
+        return response;
+    };
+});
+var dates = [
+    'Today',
+    'Tomorrow',
+    'This Week',
+    'This Weekend',
+    'Next Week',
+    'This Month',
+    'Custom Date',
+    'All Dates'
+];
