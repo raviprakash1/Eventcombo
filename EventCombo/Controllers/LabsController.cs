d9c07189889393548f4a09944351598807287af0
KT-PCLAP-026\KiwiTech <ravi.prakash@kiwitech.com>
2016-12-05 15:33:51 +0530

    Added missing links and pages in Labs

---
+++ EventCombo/Controllers/LabsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventCombo.Controllers
+{
+    public class LabsController : Controller
+    {
+        // GET: Labs
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Venture()
+        {
+            return View();
+        }
+        public ActionResult Sales()
+        {
+            return View();
+        }
+        public ActionResult Marketing()
+        {
+            return View();
+        }
+        public ActionResult DesignTechnology()
+        {
+            return View();
+        }
+        public ActionResult Content()
+        {
+            return View();
+        }
+        public ActionResult Socialmedia()
+        {
+            return View();
+        }
+    }
+}
